71680e339cbb03ed51d0c4236f44331d349e027e
Jaakko Kytölä <kytola.jaakko@gmail.com>
2021-04-09 15:40:58 +0300

    chore: install linter, husky, commitlint, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div style={{ textAlign: 'center' }}>
+    <header>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
